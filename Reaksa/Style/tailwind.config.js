2d8ebb3ec43dded889dd9019a57521dfc1d26550
Mai-Reaksa <LinReaksaitepp2021@gmail.com>
2022-01-03 23:41:23 +0700

    config tailwindcss

--- Reaksa/Style/tailwind.config.js
+++ Reaksa/Style/tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./public/index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors:{
+      
+ 
+      },
+    },
   },
   plugins: [],
 }
